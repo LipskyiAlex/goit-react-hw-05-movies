db44a5da038a275255797a26d19796292122b240
Alex <alexlipskyi@icloud.com>
2023-10-05 18:06:55 +0300

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,48 @@
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import { getMovieTrends } from '../services/theMoiveApi';
 import { MoviesList } from '../components/MoviesList/MoviesList';
 import { TitleMain } from '../components/Title/Title';
+import throttle from 'lodash.throttle';
 
 const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [fetching, setFetching] = useState(true);
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const handleScroll = throttle(e => {
+      if (fetching) return;
+
+      if (
+        e.target.documentElement.scrollHeight -
+          (e.target.documentElement.scrollTop + window.innerHeight) <
+        100
+      ) {
+        setCurrentPage(prevPage => prevPage + 1);
+      }
+    }, 1000);
+
     document.addEventListener('scroll', handleScroll);
 
     return function () {
       document.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [fetching]);
 
   useEffect(() => {
-
-    const fetchMovies = async () => {
+    const fetchedMovies = async page => {
       try {
-        const fetchedData = await getMovieTrends(currentPage);
-
-        setTrendingMovies(prevState => [...prevState, ...fetchedData]);
+        setFetching(true);
+        const fetchedData = await getMovieTrends(page);
+        setTrendingMovies(prevData => [...prevData, ...fetchedData]);
       } catch (error) {
         console.log(error.message);
+      } finally {
+        setFetching(false);
       }
     };
-    fetchMovies();
+    fetchedMovies(currentPage);
   }, [currentPage]);
-
-  const handleScroll = e => {
-    if (
-      e.target.documentElement.scrollHeight -
-        (e.target.documentElement.scrollTop + window.innerHeight) <
-      100
-    ) {
-      setCurrentPage(prev => prev + 1);
-    }
-  };
 
   return (
     <>
